b5baeeb77e257c836d3c9a6ecdcd15c79d691ef2
hupimao <toranekowg@outlook.com>
2024-03-05 18:30:17 +0800

    4.服务和存储库_连接控制器

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,27 @@
 import { Controller, Get, Post, Body, Param } from "@nestjs/common";
 import { CreateMessageDto } from "./dtos/create-message.dto";//引入CreateMessageDto 类作为参数类型
+import { MessagesService } from "./messages.service";//
 
 @Controller("messages")
 export class MessagesController {
+	messageService: MessagesService; //声明一个messageService属性
+	constructor() {
+		//注入一个MessagesService实例  真实app不这么用 会用依赖注入
+		this.messageService = new MessagesService();
+	}
+
 	@Get()
 	listMessages() {
-		return 1;
+		return this.messageService.findAll(); //调用findAll方法 返回所有消息
 	}
 	@Post()
 	createMessage(@Body() body: CreateMessageDto) {
-		console.log(body);
+		return this.messageService.create(body.content);
 	}
 
 	@Get("/:id")
 	getMessage(@Param("id") id: string) {
-		console.log(id);
+		return this.messageService.findOne(id);
 	}
 }
 
